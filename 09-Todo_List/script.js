9d69b51ef60eba3da008b49a0733b33a75ca69c7
Chiradeep-Banik <chiradeep13@gmail.com>
2021-08-17 21:47:30 +0530

    modified_the_project

--- 09-Todo_List/script.js
+++ 09-Todo_List/script.js
@@ -1,27 +1,23 @@
 const add_btn = document.querySelector('#submit');
 const input = document.querySelector('#work');
-const list = document.querySelector('#list');
-var todo = document.querySelector('.list_item');
-
+var list = document.querySelector('#list');
+var check,label,x ;
+var todo = [];
 add_btn.addEventListener('click' , ()=>{
     var val = input.value;
-    var data = `<div class="list_item">
-                    <input type="checkbox" name="checkbox" id="checkbox" value="checkbox">
-                    <label>${val}</label>
-                </div>`;
-    list.innerHTML += data;
+    const item ={
+        text : val,
+        done : false
+    };
+    todo.push(item);
+    render(list,todo);
 });
-if(todo.length){
-    todo.forEach((item)=>{
-        item.addEventListener('change', ()=>{
-            var check_box = item.querySelector('#checkbox');
-            var value = item.querySelector('label');
-            if(check_box.checked){
-                value.style.textDecoration = 'line-through';
-                console.log(value.innerHTML);
-            }else{
-                value.style.textDecoration = 'none';
-            }
-        });
-    });
+function render(list,list_items){
+    list.innerHTML = (list_items.map((item,index)=>{
+        return `<div class="list_item">
+            <input type="checkbox" name="checkbox" id="checkbox${index}" ${item.done == false ? '' : 'checked'}>
+            <label for="checkbox${index}" ${item.done == true ? 'style.textDecoration = "line-through"' : '"style.textDecoration = "none"'}>${item.text}</label>
+        </div>`
+    })).join('');
+    console.log(list_items);
 }
